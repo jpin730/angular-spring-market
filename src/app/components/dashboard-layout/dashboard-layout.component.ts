62084c27a1952b549bcb009760a9c0fdae808424
Jaime Pineda <jpin730@pm.me>
2024-03-21 12:37:04 -0600

    feat: :lipstick: add side nav layout

--- src/app/components/dashboard-layout/dashboard-layout.component.ts
+++ src/app/components/dashboard-layout/dashboard-layout.component.ts
@@ -4,14 +4,16 @@ import { RouterModule } from '@angular/router'
 import { NgIconComponent, provideIcons } from '@ng-icons/core'
 import { matMenuOutline } from '@ng-icons/material-icons/outline'
 import { PATH } from '../../constants/path.constant'
+import { ReplacePipe } from '../../pipes/replace.pipe'
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, NgIconComponent],
+  imports: [CommonModule, RouterModule, NgIconComponent, ReplacePipe],
   templateUrl: './dashboard-layout.component.html',
   viewProviders: [provideIcons({ matMenuOutline })],
 })
 export class DashboardLayoutComponent {
   path = PATH
+  sideNavOpened = false
 }
